1f5749ed341eabb3e062ae63ba629b7bb57cc63a
Srinivas <nandusrinivas33.cse@gmail.com>
2019-12-19 12:58:35 +0530

    update puma.rb with WEB_CONCURRENCY

--- config/puma.rb
+++ config/puma.rb
@@ -2,5 +2,7 @@ threads_count = (ENV['RAILS_MAX_THREADS'].presence || '5').to_i
 threads threads_count, threads_count
 port (ENV['PORT'].presence || '3000')
 environment (ENV['RAILS_ENV'].presence || 'development')
+workers (ENV.fetch("WEB_CONCURRENCY") { 2 })
+preload_app!
 
 plugin :tmp_restart
